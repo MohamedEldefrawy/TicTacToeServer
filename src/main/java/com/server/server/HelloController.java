21c04a476ba3fcbbb5812179758543087af6a0b5
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-20 22:18:59 +0200

    initial commit

---
+++ src/main/java/com/server/server/HelloController.java
@@ -0,0 +1,14 @@
+package com.server.server;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.server.server {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.server.server to javafx.fxml;
+    exports com.server.server;
+}
