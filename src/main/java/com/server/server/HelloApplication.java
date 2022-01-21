5098501cd465ed2e0dc5024f546b6d6190ff2236
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2022-01-21 17:31:47 +0200

    Creating and Linking initial css file

--- src/main/java/com/server/server/HelloApplication.java
+++ src/main/java/com/server/server/HelloApplication.java
@@ -12,11 +12,12 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/server/server/views/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        scene.getStylesheets().add(getClass().getResource("/com/server/server/css/style.css").toString());
+//        System.out.println(getClass().getResource("/com/server/server/css/style.css"));
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
