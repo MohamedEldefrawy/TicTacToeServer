901b758ca048c564f6699fcf4b19ab23b188b414
MohamedYehia <mohamed.yehia331@gmail.com>
2022-02-05 06:58:31 +0200

    handling client request part10

--- src/main/java/serverHandler/ServerHandler.java
+++ src/main/java/serverHandler/ServerHandler.java
@@ -206,7 +206,7 @@ public class ServerHandler extends Thread {
             }
 
         }
-        receiveInvitationDto = null;
+        receiveInvitationDto = new ReceiveInvitationDto();
     }
 
     public void run() {
